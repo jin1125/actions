4d27dabeb4f2b7e6bcd3279ec51168762138d588
jin1125 <jin1125@users.noreply.github.com>
2021-08-29 03:20:09 +0000

    deploy: d3cd686f18edceaa32cef90871d976abbd20fefa

--- _next/static/3_NizLh7qZzptmwhThic8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-7e30cc0c2562f9a2ae43.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/QsW_qmZ3pHTg2Jbw_pCCE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b8e496a5e05c02472b93.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
